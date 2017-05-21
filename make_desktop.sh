4e57b8fc88b0c7763f0b36087521f609424a8d0e
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-05-21 14:34:41 -0700

    Changed web and desktop builds to use same directory.

--- make_desktop.sh
+++ make_desktop.sh
@@ -8,8 +8,8 @@ CFLAGS="-O3 -g -std=c++14 -lSDL2 -lSDL2_mixer -lpthread -DDESKTOP=true"
 
 rm -f nes
 #rm -f *.o
-#rm -f -rf build_desktop
-mkdir -p build_desktop
+#rm -f -rf build/desktop
+mkdir -p build/desktop
 
 # FIXME: This wont rebuild if any of the header files changed
 # Build each C++ file into an object file. But only if the C++ file is newer.
@@ -17,12 +17,12 @@ for entry in src/*.cc; do
 	filename=$(basename "$entry")
 	filename=$(echo $filename | cut -f 1 -d '.')
 
-	if [[ src/"$filename".cc -nt build_desktop/"$filename".o ]]; then
+	if [[ src/"$filename".cc -nt build/desktop/"$filename".o ]]; then
 		echo Building "$entry" ...
-		$CC src/"$filename".cc $CFLAGS -c -o build_desktop/"$filename".o
+		$CC src/"$filename".cc $CFLAGS -c -o build/desktop/"$filename".o
 	fi
 done
 
 # Build the exe file
 echo Building exe ...
-$CC build_desktop/*.o $CFLAGS -o saltynes
+$CC build/desktop/*.o $CFLAGS -o saltynes
--- make_web.sh
+++ make_web.sh
@@ -17,8 +17,8 @@ rm -f *.wasm
 rm -f index.js
 
 #rm -f *.o
-#rm -f -rf build_web
-mkdir -p build_web
+#rm -f -rf build/web
+mkdir -p build/web
 
 # FIXME: This wont rebuild if any of the header files changed
 # Build each C++ file into an object file. But only if the C++ file is newer.
@@ -26,12 +26,12 @@ for entry in src/*.cc; do
 	filename=$(basename "$entry")
 	filename=$(echo $filename | cut -f 1 -d '.')
 
-	if [[ src/"$filename".cc -nt build_web/"$filename".o ]]; then
+	if [[ src/"$filename".cc -nt build/web/"$filename".o ]]; then
 		echo Building "$entry" ...
-		$CC src/"$filename".cc $CFLAGS -c -o build_web/"$filename".o
+		$CC src/"$filename".cc $CFLAGS -c -o build/web/"$filename".o
 	fi
 done
 
 # Build the wasm file
 echo Building WASM ...
-$CC build_web/*.o $CFLAGS -s EXPORTED_FUNCTIONS="['_main', '_toggle_sound']" -o index.js
+$CC build/web/*.o $CFLAGS -s EXPORTED_FUNCTIONS="['_main', '_toggle_sound']" -o index.js
